16b47860310cae3c784ecf8d84598850559af20a
Markus von Rüden <m.v.rueden@open-email.de>
2018-06-10 10:56:08 +0200

    USe primary vs info

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,7 +4,7 @@ $("#search-field").ghostHunter({
     onKeyUp: true,
     displaySearchInfo: true,
     includepages: true,
-    result_template : "<a href='{{link}}'><li class='search-result-item gh-search-item'>{{title}} <span class='label label-info'>{{featured}}</span> <span class='label label-default'>{{tags}}</span></li></a>",
+    result_template : "<a href='{{link}}'><li class='search-result-item gh-search-item'>{{title}} <span class='label label-primary'>{{featured}}</span> <span class='label label-default'>{{tags}}</span></li></a>",
     info_template: "<p class='search-result-item'>Number of posts found: {{amount}}</p>",
     before: function(){
         $(".search-results").removeClass("results-hide");
